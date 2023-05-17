c807c4663bd00c3d4aad12fc70e6833239235326
Adithya S Sekhar <adithya0332@gmail.com>
2023-05-17 09:59:57 +0530

    add batch download

--- termux-setup.sh
+++ termux-setup.sh
@@ -25,6 +25,7 @@ cd ~
 
 mkdir bin
 cd bin
+
 rm termux-url-opener
 
 echo "cd /sdcard/Download">>termux-url-opener
@@ -34,6 +35,14 @@ echo "exit">>termux-url-opener
 
 dos2unix termux-url-opener
 chmod +x termux-url-opener
+
+rm batch
+echo "cd /sdcard/Download">>batch
+echo "mkdir \$1">>batch
+echo "yt-dlp --ignore-errors --no-warnings --windows-filenames -f bestvideo[vcodec^=avc1][height<=144]+bestaudio[ext=m4a] %aria2% -o \"\$1\%%(title).106s-high-%%(id).10s.%%(ext)s\" -a \"\$1.txt\"">>batch
+
+dos2unix batch
+chmod +x batch
 
 cd ~
 
