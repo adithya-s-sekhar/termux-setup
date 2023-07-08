62fdb2ff8bfc02daa195f44f00a99453d33b8c81
Adithya S Sekhar <adithya0332@gmail.com>
2023-07-08 10:43:09 +0530

    display messages to user

--- termux-setup.sh
+++ termux-setup.sh
@@ -1,12 +1,21 @@
 # get storage permissions
 
+echo "Press Allow to give storage permissions"
+
 termux-setup-storage
 
 # update and install packages
 
+echo "Updating packages"
+
 yes | pkg up
 
+echo "Installing python, ffmpeg, aria2"
+
 yes | pkg install python ffmpeg aria2
+
+echo "Installing yt-dlp"
+
 pip install yt-dlp
 
 # go to home folder to prepare setup
@@ -14,18 +23,28 @@ pip install yt-dlp
 cd ~
 
 # cleaning old files
+
+echo "Cleaning old setup folder"
+
 rm -rf termux-setup
 
 # creating setup folder
+
+echo "Creating setup folder"
+
 mkdir termux-setup
 cd termux-setup
 
 # creating new scripts
 
+echo "Creating python web server script"
+
 ## python web server
 echo "cd /sdcard">>server
 echo "python -m http.server 8080">>server
 dos2unix server
+
+echo "Creating termux share menu"
 
 ## termux-url-opener
 echo "cd /sdcard/Download">>termux-url-opener
@@ -33,6 +52,8 @@ echo "yt-dlp -f bv*+ba/b --downloader aria2c -o \"%(title)100s-%(height)sp-%(id)
 echo "echo \"\$1\">>failed.txt">>termux-url-opener
 echo "exit">>termux-url-opener
 dos2unix termux-url-opener
+
+echo "Creating batch downloader"
 
 ## batchdl
 echo "cd /sdcard/Download">>batchdl
@@ -42,6 +63,8 @@ echo "cd \$1">>batchdl
 echo "yt-dlp -f bv*+ba/b --downloader aria2c -o \"%(title)s-%(height)sp-%(id)s.%(ext)s\" -a \"\$1.txt\"">>batchdl
 echo "cd ~">>batchdl
 dos2unix batchdl
+
+echo "Creating upater"
 
 ## update-setup
 
@@ -57,6 +80,8 @@ dos2unix update-setup
 
 # creating folders
 
+echo "Creating folders"
+
 mkdir ~/.shortcuts/
 mkdir ~/.shortcuts/tasks
 mkdir ~/bin
@@ -69,12 +94,16 @@ USR_BIN=~/../usr/bin
 
 # deleting old files from these folders
 
+echo "Deleting old scripts"
+
 rm $TASKS/server
 rm $BIN/termux-url-opener
 rm $USR_BIN/batchdl
 rm $USR_BIN/update-setup
 
 # copying files
+
+echo "Copying new scripts"
 
 cp server $TASKS
 cp termux-url-opener $BIN
@@ -83,12 +112,16 @@ cp update-setup $USR_BIN
 
 # setting permissions
 
+echo "Settings permissions"
+
 chmod +x $TASKS/server
 chmod +x $BIN/termux-url-opener
 chmod +x $USR_BIN/batchdl
 chmod +x $USR_BIN/update-setup
 
 # cleaning up install folder
+
+echo "Cleaning up"
 
 cd ~
 rm -rf termux-setup
