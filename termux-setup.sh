c2623f2d27750436b53432536dec663fc9563e36
Adithya S Sekhar <adithya0332@gmail.com>
2023-09-27 08:21:25 +0530

    Update termux-setup.sh

--- termux-setup.sh
+++ termux-setup.sh
@@ -28,7 +28,7 @@ echo "python -m http.server 8080">>$TASKS/server
 ## termux-url-opener
 echo "Creating termux share menu"
 echo "cwd=\$(pwd)">$BIN/termux-url-opener
-echo "mkdir /sdcard/Download/Termux">$BIN/termux-url-opener
+echo "mkdir -p /sdcard/Download/Termux">$BIN/termux-url-opener
 echo "yt-dlp -f bv*+ba/b --windows-filenames --embed-chapters --no-mtime -P temp:\"\$cwd\" -P home:\"/sdcard/Download/Termux\" -o \"%(title)s-%(id)s.%(ext)s\" \"\$1\" && echo \"\$1\">>/sdcard/Download/Termux/downloaded.txt && exit">>$BIN/termux-url-opener
 echo "echo \"\$1\">>/sdcard/Download/Termux/failed.txt">>$BIN/termux-url-opener
 echo "exit">>$BIN/termux-url-opener
@@ -46,4 +46,4 @@ chmod +x $USR_BIN/update-setup
 # finished
 echo "Installed Successfully"
 echo
-echo "The script was last updated on 2023-Sep-27 08:12AM IST."
+echo "The script was last updated on 2023-Sep-27 08:21AM IST."
